50a42eeabd20e95ceaf379599956a8240e0b6151
togiapr <togi.apr@gmail.com>
2022-02-13 13:21:50 +0700

    Layouting

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
       fontFamily:{
         inter:['Inter']
       },
-    },
-  },
+      animation: {
+                'spin-slow': 'spin 3s linear infinite',
+                'goyang':'goyang 1s ease-in-out infinite'
+              },
+              keyframes: {
+                goyang:{
+                  '0%, 100%':{
+                    transform:'rotate(-3deg)'
+                  },
+                  '50%':{
+                    transform:'rotate(3deg)'
+                  },
+                }
+              },
+            },
+          },
   plugins: [],
 }
